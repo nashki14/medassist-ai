1f90bc3cb332825db8e53f6453f89e6183e9fd4b
nashki14 <156869089+nashki14@users.noreply.github.com>
2026-04-30 09:49:57 +0700

    fix: add api timeout config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+}
 module.exports = nextConfig
